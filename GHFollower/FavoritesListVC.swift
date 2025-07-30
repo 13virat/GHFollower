6c14e498eae567f3c633bd7c74c8b4191eb00936
Virat Gupta <viratgupta1314@gmail.com>
2025-07-31 02:21:09 +0530

    TabBar

---
+++ GHFollower/FavoritesListVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListVC.swift
+//  GHFollower
+//
+//  Created by Mac pro on 31/07/25.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollower/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GHFollower
+//
+//  Created by Mac pro on 31/07/25.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
