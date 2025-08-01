9a70f4b53f315fdd078aec53802c3d7549be3a97
Virat Gupta <viratgupta1314@gmail.com>
2025-08-02 03:25:11 +0530

    Structure + Follower API

---
+++ GHFollower/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GHFollower
+//
+//  Created by Mac pro on 01/08/25.
+//
+
+import Foundation
+struct Follower: Codable{
+    var login: String
+    var avatarUrl: String
+}
+
---
+++ GHFollower/Screens/FollowerListVC.swift
@@ -0,0 +1,30 @@
+//
+//  FollowerListVC.swift
+//  GHFollower
+//
+//  Created by Mac pro on 31/07/25.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+        NetworkManager.shared.getFollower(for: username, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad Stuff Happend", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            print("Follower.count = \(followers.count)")
+            print(followers)
+        }
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)    }
+}
