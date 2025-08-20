67fe96847548221c9c2587c288b2ce64be3f242b
Virat Gupta <viratgupta1314@gmail.com>
2025-08-21 01:49:57 +0530

    userinfo viewcontroller

---
+++ GHFollower/Screens/UserInfoVC.swift
@@ -0,0 +1,25 @@
+//
+//  UserInfoVC.swift
+//  GHFollower
+//
+//  Created by Mac pro on 20/08/25.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+
+    var username: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
+        navigationItem.rightBarButtonItem = doneButton
+        print(username!)
+    }
+    
+    @objc func dismissVC(){
+        dismiss(animated: true)
+    }
+}
